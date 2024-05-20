156fbd1af462b8b26b909aba97c5031902c21b92
Jacob <jcpguanzing@gmail.com>
2024-05-20 21:28:17 +0800

    .

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
 import todoReducer from "@/features/todos/todoSlice";
 import thunkMiddleware from "redux-thunk";
 const store = configureStore({
     reducer: {
         todos: todoReducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunkMiddleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        thunk: {
+            extraArgument: thunkMiddleware
+        }
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
